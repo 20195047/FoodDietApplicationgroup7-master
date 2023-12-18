39042092b90e6e30d4b0cfe683938429061687f0
Ben <84084934+20195047@users.noreply.github.com>
2023-12-18 20:43:45 +0800

    Delete FoodDietApplicationgroup7-master directory

--- FoodDietApplicationgroup7-master/app/src/main/java/com/health/fooddietapplication/adapters/UpdateVerticalRec.java
+++
@@ -1,9 +0,0 @@
-package com.health.fooddietapplication.adapters;
-
-import com.health.fooddietapplication.models.HomeVerModel;
-
-import java.util.ArrayList;
-
-public interface UpdateVerticalRec {
-    public void callBack(int position, ArrayList<HomeVerModel> list);
-}
